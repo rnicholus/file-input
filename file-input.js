32793289cf607240b7fb3a9513839602c1b6cb9f
Ray Nicholus <rnicholus+github@widen.com>
2014-06-25 00:26:12 -0400

    feat($extension): optional file extensions validation
    Allow users to specify an array of allowed or disallowed extensions.
    
    closes #7

--- file-input.js
+++ file-input.js
@@ -1,13 +1,45 @@
-var getResultOfCountLimitValidation = function(limit, files) {
-    if (limit > 0 && limit < files.length) {
-        return {
-            invalid: files.slice(limit, files.length),
-            valid: files.slice(0, limit)
-        }        
+var getLowerCaseExtension = function(filename) {
+        var extIdx = filename.lastIndexOf(".") + 1;
+
+        if (extIdx > 0) {
+            return filename.substr(extIdx, filename.length - extIdx).toLowerCase();
+        }
+    },
+    
+    getResultOfCountLimitValidation = function(limit, files) {
+        if (limit > 0 && limit < files.length) {
+            return {
+                invalid: files.slice(limit, files.length),
+                valid: files.slice(0, limit)
+            }        
+        }
+    
+        return {invalid: [], valid: files};
+    },
+
+    getResultOfExtensionsValidation = function(extensionsStr, files) {
+        if (extensionsStr) {
+            var negate = extensionsStr.charAt(0) === "!",
+                extensions = JSON.parse(extensionsStr.toLowerCase().substr(negate ? 1 : 0)),
+                result = {invalid: [], valid: []};
+
+            files.forEach(function(file) {
+                var extension = getLowerCaseExtension(file.name);
+                
+                if (extensions.indexOf(extension) >= 0) {
+                    result[negate ? "invalid" : "valid"].push(file);
+                }
+                else {
+                    result[negate? "valid" : "invalid"].push(file);
+                }
+            });
+            
+            return result;
+        }   
+        
+        return {invalid: [], valid: files};
     }
 
-    return {invalid: [], valid: files};
-};
 
 /* globals Polymer */
 Polymer("file-input", {
@@ -16,8 +48,13 @@ Polymer("file-input", {
             invalid = {count: 0},
             valid = [];
         
-        var countLimitValidationResult = getResultOfCountLimitValidation(this.maxFiles, files);
+        var extensionValidationResult = getResultOfExtensionsValidation(this.extensions, files);
+        var countLimitValidationResult = getResultOfCountLimitValidation(this.maxFiles, extensionValidationResult.valid);
 
+        if (extensionValidationResult.invalid.length) {
+            invalid["badExtensions"] = extensionValidationResult.invalid;
+            invalid.count += extensionValidationResult.invalid.length;
+        }
         if (countLimitValidationResult.invalid.length) {
             invalid["tooMany"] = countLimitValidationResult.invalid;
             invalid.count += countLimitValidationResult.invalid.length;
