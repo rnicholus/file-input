1928d8f77c982817d4c637edf66d69b41f4c4690
Ray Nicholus <rnicholus+github@widen.com>
2014-06-26 03:15:34 +0000

    feat($size-validation): allow filtering by file size
    min & max file size validation
    
    closes #11

--- file-input.js
+++ file-input.js
@@ -5,15 +5,15 @@ var getLowerCaseExtension = function(filename) {
             return filename.substr(extIdx, filename.length - extIdx).toLowerCase();
         }
     },
-    
+
     getResultOfCountLimitValidation = function(limit, files) {
         if (limit > 0 && limit < files.length) {
             return {
                 invalid: files.slice(limit, files.length),
                 valid: files.slice(0, limit)
-            }        
+            }
         }
-    
+
         return {invalid: [], valid: files};
     },
 
@@ -25,7 +25,7 @@ var getLowerCaseExtension = function(filename) {
 
             files.forEach(function(file) {
                 var extension = getLowerCaseExtension(file.name);
-                
+
                 if (extensions.indexOf(extension) >= 0) {
                     result[negate ? "invalid" : "valid"].push(file);
                 }
@@ -33,12 +33,36 @@ var getLowerCaseExtension = function(filename) {
                     result[negate? "valid" : "invalid"].push(file);
                 }
             });
-            
+
             return result;
-        }   
-        
+        }
+
         return {invalid: [], valid: files};
-    }
+    },
+
+    getResultOfSizeValidation = function(minSize, maxSize, files) {
+        if (!minSize && !maxSize) {
+            return {tooBig: [], tooSmall: [], valid: files};
+        }
+
+        var valid = []
+            tooBig = [],
+            tooSmall = [];
+
+        files.forEach(function(file) {
+            if (minSize && file.size < minSize) {
+                tooSmall.push(file);
+            }
+            else if (maxSize && file.size > maxSize) {
+                tooBig.push(file);
+            }
+            else {
+                valid.push(file);
+            }
+        });
+
+        return {tooBig: tooBig, tooSmall: tooSmall, valid: valid};
+    };
 
 
 /* globals Polymer */
@@ -47,10 +71,19 @@ Polymer("file-input", {
         var files = Array.prototype.slice.call(this.$.fileInputInput.files),
             invalid = {count: 0},
             valid = [];
-        
-        var extensionValidationResult = getResultOfExtensionsValidation(this.extensions, files);
+
+        var sizeValidationResult = getResultOfSizeValidation(this.minSize, this.maxSize, files);
+        var extensionValidationResult = getResultOfExtensionsValidation(this.extensions, sizeValidationResult.valid);
         var countLimitValidationResult = getResultOfCountLimitValidation(this.maxFiles, extensionValidationResult.valid);
 
+        if (sizeValidationResult.tooBig.length) {
+            invalid["tooBig"] = sizeValidationResult.tooBig;
+            invalid.count += sizeValidationResult.tooBig.length;
+        }
+        if (sizeValidationResult.tooSmall.length) {
+            invalid["tooSmall"] = sizeValidationResult.tooSmall;
+            invalid.count += sizeValidationResult.tooSmall.length;
+        }
         if (extensionValidationResult.invalid.length) {
             invalid["badExtensions"] = extensionValidationResult.invalid;
             invalid.count += extensionValidationResult.invalid.length;
@@ -59,28 +92,28 @@ Polymer("file-input", {
             invalid["tooMany"] = countLimitValidationResult.invalid;
             invalid.count += countLimitValidationResult.invalid.length;
         }
-        
+
         valid = countLimitValidationResult.valid;
-        
+
         this.invalidFiles = invalid;
         this.files = valid;
-        
+
         this.fire("change", {invalid: invalid, valid: valid});
     },
-    
+
     directory: false,
-    
+
     files: [],
-    
+
     invalidFiles: {count: 0},
-    
+
     maxFiles: 0,
 
     ready: function() {
         if (this.maxFiles !== 1) {
-            this.$.fileInputInput.setAttribute("multiple", "");    
+            this.$.fileInputInput.setAttribute("multiple", "");
         }
-        
+
         if (this.directory && this.$.fileInputInput.webkitdirectory !== undefined) {
             this.$.fileInputInput.setAttribute("webkitdirectory", "");
         }
